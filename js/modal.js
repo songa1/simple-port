2cfa184a970bf4bda999f87b0f9026ccb91ad904
songa1 <songachillethe1her@gmail.com>
2021-10-27 15:32:41 +0200

    I have finished almost everything

--- js/modal.js
+++ js/modal.js
@@ -8,9 +8,15 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
+const logo = document.getElementById("logo");
+
 btn.onclick = function() {
   modal.style.display = "block";
 }
+
+logo.addEventListener("mouseover", ()=> {
+    modal.style.display = "block";
+})
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
