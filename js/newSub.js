017daa21c84f474d2da627ffcaa997773edb1019
songa1 <songachillethe1her@gmail.com>
2021-10-29 13:07:33 +0200

    Added a feedback

--- js/newSub.js
+++ js/newSub.js
@@ -1,6 +1,7 @@
 const submitBtn = document.getElementById('submit');
 const email = document.getElementById('email');
 const nameValue = document.getElementById('name');
+let fb = document.querySelector('.feedback');
 
 submitBtn.addEventListener('click', (e)=> {
     e.preventDefault();
@@ -16,5 +17,21 @@ submitBtn.addEventListener('click', (e)=> {
         })
     })
     .then(res => res.json())
-    .then(data=> console.log(data.name))
+    .then(data=> {
+        fb.style.color = 'white';
+        fb.style.backgroundColor = 'green';
+        fb.style.padding = '10px';
+        fb.style.borderRadius = '5px';
+        fb.style.textAlign = 'center';
+        submitBtn.style.display = 'none'
+        email.style.display = 'none';
+        nameValue.style.display = 'none';
+        email.disabled = 'true'
+        nameValue.disabled = 'true'
+        fb.innerHTML = `Hi ${data.name}! Thank you for becoming my friend! Check your email for my gift.`
+        setTimeout(() => {
+            fb.style.display = 'none';
+            
+        }, 5000);
+    })
 })
